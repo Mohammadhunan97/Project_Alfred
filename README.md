5f66b91b93a411200c57a9289962a247b3559d34
Mohammad Chughtai <mohammadhunan@gmail.com>
2017-04-06 16:36:25 -0400

    createuser

--- README.md
+++
@@ -1,7 +0,0 @@
-# Project_Alfred
-Project Alfred is a CRUD application using MVC architecture built with Full Stack Javascript
-
-
---------
-
-Project Alfred is a voice assistant designed for quickly recieving information
--- schema.sql
+++ schema.sql
@@ -1,25 +1,37 @@
--- createdb alfred
--- psql -d alfred -f schema.sql
+--createdb alfred_teams
 DROP TABLE IF EXISTS users CASCADE;
-DROP TABLE IF EXISTS events CASCADE;
+DROP TABLE IF EXISTS teams CASCADE;
+DROP TABLE IF EXISTS subforums CASCADE;
+DROP TABLE IF EXISTS posts CASCADE;
+
+CREATE TABLE teams(
+team_id SERIAL,
+team_name VARCHAR(20) PRIMARY KEY NOT NULL,
+team_password VARCHAR(255)
+);
 
 CREATE TABLE users(
-id SERIAL PRIMARY KEY,
-name VARCHAR(20),
-password VARCHAR(20),
-email VARCHAR(40)
+usr_id SERIAL PRIMARY KEY,
+user_name VARCHAR(20) NOT NULL,
+user_password VARCHAR(255) NOT NULL,
+user_description TEXT,
+profile_image TEXT,
+ref_team_name_users VARCHAR(20), FOREIGN KEY(ref_team_name_users) references teams(team_name)
 );
 
-CREATE TABLE events(
-event_id SERIAL PRIMARY KEY,
+CREATE TABLE subforums(
+subforum_id SERIAL,
+subforum_name VARCHAR(20) PRIMARY KEY NOT NULL,
+subforum_description TEXT,
+subforum_password VARCHAR(255),
+ref_team_id_subforums VARCHAR(20), FOREIGN KEY(ref_team_id_subforums) references teams(team_name)
+);
+
+CREATE TABLE posts(
+post_id SERIAL PRIMARY KEY,
 title VARCHAR(20),
-description TEXT,
-time VARCHAR(10),
-user_id INT, FOREIGN KEY(user_id) references users(id)
+post_description TEXT,
+tags TEXT, --this is an stringed array which we are using to later retrieve the post from an archive
+ref_team_id_posts VARCHAR(20), FOREIGN KEY(ref_team_id_posts) references teams(team_name),
+ref_subforum_name VARCHAR(20), FOREIGN KEY(ref_subforum_name) references subforums(subforum_name)
 );
-
---sample data
-INSERT INTO users (name,password,email) VALUES ('George Washington','foobar','mohammad.chughtai@gmail.com');
-INSERT INTO users (name,password,email) VALUES ('Tom Washington','foobar','mohammad.chughtai@gmail.com');
-INSERT INTO events(title, description, time, user_id)VALUES('oobar','foobar','3:00pm',1);
-INSERT INTO events (title, description, time, user_id) VALUES ('foo event 2', 'something more interesting foobar', '5:00pm', 2); 
